79d5d9c73cf61f6cc8bea6ee77e46518279f0388
Jaysen Spurlock <jayspur11@gmail.com>
2023-04-08 15:58:28 -0700

    add some notecard layout!

---
+++ cognit/webapp/src/features/notecard/Notecard.tsx
@@ -0,0 +1,29 @@
+import styles from "./Notecard.module.css"
+
+type NotecardProps = {
+    title: string;
+    preview: string;
+    cardTapped: VoidFunction;
+};
+
+export function Notecard(props: NotecardProps) {
+    let { title, preview, cardTapped } = props;
+
+    return (
+        <div
+            className={styles.card}
+            onClick={cardTapped}
+        >
+            {title.length &&
+                <div className={styles.title}>
+                    {title}
+                </div>
+            }
+            {preview.length &&
+                <div className={styles.preview}>
+                    {preview}
+                </div>
+            }
+        </div>
+    );
+}
